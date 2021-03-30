e39940d54dbca0257bb291d5f42d8e632e658b47
kfield1 <kayt1303@yahoo.com>
2021-03-29 20:57:43 -0500

    import starter

---
+++ app.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")

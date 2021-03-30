35150a6b12c082b171a807e6181d7985d44b16b4
kfield1 <kayt1303@yahoo.com>
2021-03-29 21:00:28 -0500

    starting flask

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ from flask import Flask, jsonify
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
